504314c879d7f5a9832cc325934934be504582ff
jackYao777 <18772869253@163.com>
2024-08-28 09:38:25 +0800

    axios配置初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 // import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
+import { getCategory } from '@/apis/testApi.js'
+
+getCategory().then(res=>{
+    console.log(res)
+})
 const app= createApp(App)
 const pinia = createPinia()
 // pinia.use(piniaPluginPersistedstate)
