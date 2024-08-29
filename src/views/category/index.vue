bcc53f83f99794a54302198d7120b4cb2103cc97
jackYao777 <18772869253@163.com>
2024-08-29 09:37:04 +0800

    解决router缓存问题以及路由守卫实现局部更新

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,163 +1,171 @@
 <script setup>
 import { getCategoryAPI } from '@/apis/category';
-import {onMounted, onUpdated, ref} from 'vue'
+import { onMounted, onUpdated, ref } from 'vue'
 import { useRoute } from 'vue-router';
 import { getBannerAPI } from '@/apis/home'
 import GoodsItem from '@/views/home/components/GoodsItem.vue';
+import { onBeforeRouteUpdate } from 'vue-router'
 //面包屑
-const categoryData=ref({})
+const categoryData = ref({})
 const route=useRoute()
-const getCategory= async ()=>{
-    const res=await getCategoryAPI(route.params.id)
-    categoryData.value=res.data.result
-    // console.log('sdfsdf',categoryData);
+//route.params.id 为id的默认参数，当下面的onMounted调用时使用
+const getCategory = async (id = route.params.id) => {
+    const res = await getCategoryAPI(id)
+    categoryData.value = res.data.result
 }
+
+
+//解决路由缓存
+//这里的to指的是目标路由
+onBeforeRouteUpdate((to) => {
+    getCategory(to.params.id)
+})
+
 
 //轮播图片
 const bannerList = ref([])
 const getBanner = async () => {
-  const res = await getBannerAPI({ distributionSite: '2' })
-  bannerList.value = res.data.result
+    const res = await getBannerAPI({ distributionSite: '2' })
+    bannerList.value = res.data.result
 }
 onMounted(() => getBanner())
-// onMounted(()=>getCategory())
-onUpdated(()=>getCategory())
+onMounted(() => getCategory())
 
 </script>
 
 <template>
-  <div class="top-category">
-    <div class="container m-top-20">
-      <!-- 面包屑 -->
-      <div class="bread-container">
-        <el-breadcrumb separator=">">
-          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
-        </el-breadcrumb>
-      </div>
+    <div class="top-category">
+        <div class="container m-top-20">
+            <!-- 面包屑 -->
+            <div class="bread-container">
+                <el-breadcrumb separator=">">
+                    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                    <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
+                </el-breadcrumb>
+            </div>
 
-        <!-- 轮播图 -->
-        <div class="home-banner">
-        <el-carousel height="500px">
-          <el-carousel-item v-for="item in bannerList" :key="item.id">
-            <img :src="item.imgUrl" alt="">
-          </el-carousel-item>
-        </el-carousel>
-      </div>
+            <!-- 轮播图 -->
+            <div class="home-banner">
+                <el-carousel height="500px">
+                    <el-carousel-item v-for="item in bannerList" :key="item.id">
+                        <img :src="item.imgUrl" alt="">
+                    </el-carousel-item>
+                </el-carousel>
+            </div>
 
-       <!-- 分类数据 -->
-       <div class="sub-list">
-        <h3>全部分类</h3>
-        <ul>
-          <li v-for="i in categoryData.children" :key="i.id">
-            <RouterLink :to="`/category/sub/${i.id}`">
-              <img :src="i.picture" />
-              <p>{{ i.name }}</p>
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
-      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
-        <div class="head">
-          <h3>- {{ item.name }}-</h3>
+            <!-- 分类数据 -->
+            <div class="sub-list">
+                <h3>全部分类</h3>
+                <ul>
+                    <li v-for="i in categoryData.children" :key="i.id">
+                        <RouterLink :to="`/category/sub/${i.id}`">
+                            <img :src="i.picture" />
+                            <p>{{ i.name }}</p>
+                        </RouterLink>
+                    </li>
+                </ul>
+            </div>
+            <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+                <div class="head">
+                    <h3>- {{ item.name }}-</h3>
+                </div>
+                <div class="body">
+                    <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+                </div>
+            </div>
+
         </div>
-        <div class="body">
-          <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
-        </div>
-      </div>
-
     </div>
-  </div>
 </template>
 
 
 <style scoped lang="scss">
 .home-banner {
-  width: 1240px;
-  height: 500px;
-  margin: 0 auto;
-
-  img {
-    width: 100%;
+    width: 1240px;
     height: 500px;
-  }
+    margin: 0 auto;
+
+    img {
+        width: 100%;
+        height: 500px;
+    }
 }
 
 .top-category {
-  h3 {
-    font-size: 28px;
-    color: #666;
-    font-weight: normal;
-    text-align: center;
-    line-height: 100px;
-  }
-
-  .sub-list {
-    margin-top: 20px;
-    background-color: #fff;
-
-    ul {
-      display: flex;
-      padding: 0 32px;
-      flex-wrap: wrap;
-
-      li {
-        width: 168px;
-        height: 160px;
-
-
-        a {
-          text-align: center;
-          display: block;
-          font-size: 16px;
-
-          img {
-            width: 100px;
-            height: 100px;
-          }
-
-          p {
-            line-height: 40px;
-          }
-
-          &:hover {
-            color: $xtxColor;
-          }
-        }
-      }
-    }
-  }
-
-  .ref-goods {
-    background-color: #fff;
-    margin-top: 20px;
-    position: relative;
-
-    .head {
-      .xtx-more {
-        position: absolute;
-        top: 20px;
-        right: 20px;
-      }
-
-      .tag {
+    h3 {
+        font-size: 28px;
+        color: #666;
+        font-weight: normal;
         text-align: center;
-        color: #999;
-        font-size: 20px;
+        line-height: 100px;
+    }
+
+    .sub-list {
+        margin-top: 20px;
+        background-color: #fff;
+
+        ul {
+            display: flex;
+            padding: 0 32px;
+            flex-wrap: wrap;
+
+            li {
+                width: 168px;
+                height: 160px;
+
+
+                a {
+                    text-align: center;
+                    display: block;
+                    font-size: 16px;
+
+                    img {
+                        width: 100px;
+                        height: 100px;
+                    }
+
+                    p {
+                        line-height: 40px;
+                    }
+
+                    &:hover {
+                        color: $xtxColor;
+                    }
+                }
+            }
+        }
+    }
+
+    .ref-goods {
+        background-color: #fff;
+        margin-top: 20px;
         position: relative;
-        top: -20px;
-      }
+
+        .head {
+            .xtx-more {
+                position: absolute;
+                top: 20px;
+                right: 20px;
+            }
+
+            .tag {
+                text-align: center;
+                color: #999;
+                font-size: 20px;
+                position: relative;
+                top: -20px;
+            }
+        }
+
+        .body {
+            display: flex;
+            justify-content: space-around;
+            padding: 0 40px 30px;
+        }
     }
 
-    .body {
-      display: flex;
-      justify-content: space-around;
-      padding: 0 40px 30px;
+    .bread-container {
+        padding: 25px 0;
     }
-  }
-
-  .bread-container {
-    padding: 25px 0;
-  }
 }
 </style>
