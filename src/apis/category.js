223b97ae441f468b29c4590eac15fc5af82963fc
jackYao777 <18772869253@163.com>
2024-08-29 10:11:11 +0800

    二级导航面包屑实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -9,3 +9,29 @@ export function getCategoryAPI(id) {
     }
   })
 }
+
+//获取二级分类列表数据
+export function getCategoryFilterAPI(id) {
+    return httpInstance({
+      url: '/category/sub/filter',
+      params: {
+        id
+      }
+    })
+  }
+
+  /** 获取导航数据
+ * * @data { 
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   } 
+ */
+export function getSubCategoryAPI(data) {
+    return httpInstance({
+      url: '/category/goods/temporary',
+      method: 'POST',
+      data
+    })
+  }
--- src/views/subCategory/index.vue
+++ src/views/subCategory/index.vue
@@ -1,5 +1,88 @@
- <template>
-    <div>
-        二级导航数据
+<script setup>
+import { getCategoryFilterAPI, getSubCategoryAPI } from '@/apis/category'
+import { useRoute } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import GoodsItem from '../home/components/GoodsItem.vue';
+
+const categoryFilter = ref({})
+const route = useRoute()
+const getCategoryFilter = async () => {
+  const res = await getCategoryFilterAPI(route.params.id)
+  categoryFilter.value = res.data.result
+}
+onMounted(() => getCategoryFilter())
+
+</script>
+
+<template>
+  <div class="container ">
+    <!-- 面包屑 -->
+    <div class="bread-container">
+      <el-breadcrumb separator=">">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: `/category/${categoryFilter.parentId}` }">{{ categoryFilter.parentName }}
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>{{ categoryFilter.name }}</el-breadcrumb-item>
+      </el-breadcrumb>
     </div>
- </template>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.bread-container {
+  padding: 25px 0;
+  color: #666;
+}
+
+.sub-container {
+  padding: 20px 10px;
+  background-color: #fff;
+
+  .body {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 10px;
+  }
+
+  .goods-item {
+    display: block;
+    width: 220px;
+    margin-right: 20px;
+    padding: 20px 30px;
+    text-align: center;
+
+    img {
+      width: 160px;
+      height: 160px;
+    }
+
+    p {
+      padding-top: 10px;
+    }
+
+    .name {
+      font-size: 16px;
+    }
+
+    .desc {
+      color: #999;
+      height: 29px;
+    }
+
+    .price {
+      color: $priceColor;
+      font-size: 20px;
+    }
+  }
+
+  .pagination-container {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+  }
+
+
+}
+</style>
