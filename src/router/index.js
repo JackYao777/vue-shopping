491573890b654ced12346601e4cc633b394ac327
jackYao777 <18772869253@163.com>
2024-08-29 10:42:58 +0800

    人气推荐和新鲜好物路由跳转详情页面配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '@/views/login/index.vue'
 import Layout from '@/views/layout/index.vue'
 import Category from '@/views/category/index.vue'
 import SubCategory from '@/views/subCategory/index.vue'
+import Detail from '@/views/detail/index.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,10 @@ const router = createRouter({
                 {
                     path: 'category/sub/:id',
                     component: SubCategory
+                },
+                {
+                    path: 'detail/:id',
+                    component: Detail
                 },
             ]
         },
