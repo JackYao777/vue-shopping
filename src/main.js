d60f8ddf465557f796bff0f107d9da8cb4cad30d
jackYao777 <18772869253@163.com>
2024-08-30 08:51:50 +0800

    详情页面的实现

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import { lazyPlugin } from './directives'
+import { componentPlugin } from '@/components/index'
 // import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import { getCategory } from '@/apis/testApi.js'
@@ -18,4 +19,5 @@ const pinia = createPinia()
 app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
