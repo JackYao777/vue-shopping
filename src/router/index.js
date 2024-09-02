076e5718bbd6ddca010dbe1d721d136338a2cccb
jackYao777 <18772869253@163.com>
2024-09-02 10:13:38 +0800

    支付宝交互操作

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import SubCategory from '@/views/subCategory/index.vue'
 import Detail from '@/views/detail/index.vue'
 import CartList from '@/views/cartList/index.vue'
 import CheckOut from '@/views/checkout/index.vue'
+import Pay from '@/views/pay/index.vue'
+import PayBack from '@/views/pay/PayBack.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +40,14 @@ const router = createRouter({
                 {
                     path: 'checkout',
                     component: CheckOut
+                },
+                {
+                    path: 'pay',
+                    component: Pay
+                },
+                {
+                    path: 'paycallback',
+                    component: PayBack
                 },
             ]
         },
